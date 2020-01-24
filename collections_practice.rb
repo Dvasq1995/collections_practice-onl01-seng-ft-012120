40fea4dabe621b0eae4a3da2b9050accd293ff33
Diego Vasquez <botellongo69@outlook.com>
2020-01-24 23:09:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,5 +41,5 @@ def sum_array(intArr)
 end
 
 def add_s(strArr)
-  strArr.each_with_index.collect{|element, index| strArr.index(element) != 1}
+  strArr.each_with_index.collect{|element, index| strArr.index(element) != 1 ? element << 's'}
 end
